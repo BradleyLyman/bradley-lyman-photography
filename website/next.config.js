a9496b25ca56343f84fe6a2016b0d3b63cc67fb5
Bradley Lyman <lyman.brad3211@gmail.com>
2023-03-07 19:43:26 -0700

    setup basePath and assetPath for dev and gh-actions

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || "";
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions.length > 0) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/bradley-lyman-photography",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   images: {
     unoptimized: true,
