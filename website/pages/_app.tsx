bc7d7b133c1bb9ba2885444f24149fc7506e83ca
Bradley Lyman <lyman.brad3211@gmail.com>
2023-03-08 17:10:49 -0700

    remove default stuff and add favicon + webmanifest

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Noto_Sans, Montserrat } from "next/font/google";
+
+const montserrat_font = Montserrat({ weight: "600", subsets: ["latin-ext"] });
+const noto_font = Noto_Sans({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          --normal-font: ${noto_font.style.fontFamily};
+          --title-font: ${montserrat_font.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- website/pages/album/some_album.tsx
+++ website/pages/album/some_album.tsx
@@ -1,21 +1,17 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
-export default function Home() {
+export default function Album() {
   return (
     <>
       <Head>
-        <title>Bradley Lyman Photography</title>
-        <meta
-          name="description"
-          content="Bradley Lyman's photography archive."
-        />
+        <title>My Album</title>
+        <meta name="description" content="An album" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
-        <link rel="manifest" href="site.webmanifest" />
       </Head>
       <main className={styles.main}>
-        <h1>Bradley Lyman's Photography Archive</h1>
+        <h1>My Album</h1>
         <p>aoeu</p>
       </main>
     </>
